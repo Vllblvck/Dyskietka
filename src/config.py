8d4baaeab4c7559c320e6c0e5066a90eca21ca7a
vllblvck <m.komar12121998@gmail.com>
2021-08-18 19:06:28 +0200

    Moved to src directory and removed configs not intended for changes

---
+++ src/config.py
@@ -0,0 +1,5 @@
+# List containing tuples with a path to a file and google drive directory id
+# Paths can contain patterns according to the rules used by UNIX shell
+# Use 'root' for top directory
+BACKUPS = [('/example/path/to/example/file', 'invalidDirId'),
+           ('/example/path/to/example/file2', 'invalidDirId2')]
