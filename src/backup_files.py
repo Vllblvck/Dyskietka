0c1efac99ea941debb812d25e69808e4cd27c30b
vllblvck <m.komar12121998@gmail.com>
2021-09-13 12:33:34 +0200

    Added validation whether file was modified before update

--- src/backup_files.py
+++ src/backup_files.py
@@ -3,33 +3,70 @@
 import config
 import googledrive as drive
 
+from datetime import datetime, timezone
 from glob import glob
 from pathlib import Path
 
 
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
-
 AUTH_DIR = Path(__file__).parent / '..' / 'auth'
 CREDENTIALS = AUTH_DIR / 'credentials.json'
 TOKEN = AUTH_DIR / 'token.json'
+DATE_FORMAT = '%Y-%m-%dT%H:%M:%S.%f%z'
 
 
-def upload_files(drive_service, paths, directory):
+def get_newest_file(drive_files):
+    if not drive_files:
+        return None
+
+    dates_list = []
+    for file in drive_files:
+        modified_time = datetime.strptime(
+            file.get('modifiedTime'), DATE_FORMAT)
+        dates_list.append(modified_time)
+
+    dates_list.sort()
+
+    for file in drive_files:
+        modified_time = datetime.strptime(
+            file.get('modifiedTime'), DATE_FORMAT)
+        if modified_time == dates_list[-1]:
+            return file
+
+
+def file_changed(file_path, drive_file):
+    file_metadata = Path(file_path).stat()
+
+    size_changed = int(drive_file.get('size')) != file_metadata.st_size
+    if size_changed:
+        return True
+
+    local_mtime = datetime.fromtimestamp(file_metadata.st_mtime, tz=timezone.utc)
+    drive_mtime = datetime.strptime(drive_file.get('modifiedTime'), DATE_FORMAT)
+
+    return local_mtime > drive_mtime
+
+
+def upload_files(drive_svc, paths, directory):
     for path in paths:
-        # TODO Instead of skipping directories create their structure on google drive
         if Path(path).is_dir():
             continue
 
         print('Checking whether file exists on google drive...')
         file_name = Path(path).name
-        file_id = drive.get_newest_file_id(drive_service, file_name, directory)
-        if not file_id:
+        files_svc = drive.DriveFilesSvc(drive_svc)
+        drive_files = files_svc.get_files_by_name(file_name, directory)
+        drive_file = get_newest_file(drive_files)
+        drive_file_id = drive_file.get('id')
+
+        if not drive_file_id:
             print(f'Creating {file_name} on google drive...')
-            drive.create_file(drive_service, path,
-                              directory=directory)
-        else:
+            files_svc.create_file(path, directory=directory)
+        elif file_changed(path, drive_file):
             print(f'Updating {file_name} on google drive...')
-            drive.update_file(drive_service, path, file_id)
+            files_svc.update_file(path, drive_file_id)
+        else:
+            print('Skipping file update because it has not been modified')
 
 
 def main():
@@ -43,8 +80,7 @@ def main():
                 print(f'PATTERN \'{backup[0]}\' IS INVALID!!!')
                 continue
 
-            directory = backup[1]
-            upload_files(drive_service, resolved_paths, directory)
+            upload_files(drive_service, resolved_paths, backup[1])
 
         print('Done')
 
--- src/googledrive.py
+++ src/googledrive.py
@@ -1,6 +1,5 @@
 #!/usr/bin/python
 
-from datetime import datetime
 from pathlib import Path
 
 from google.auth.transport.requests import Request
@@ -10,7 +9,7 @@ from googleapiclient.http import MediaFileUpload
 from google.oauth2.credentials import Credentials
 
 
-def authorize(token_path, credentials_path, scopes) -> Resource:
+def authorize(token_path, credentials_path, scopes):
     creds = None
 
     if Path(token_path).exists():
@@ -30,65 +29,49 @@ def authorize(token_path, credentials_path, scopes) -> Resource:
     return build('drive', 'v3', credentials=creds)
 
 
-def get_files_by_name(drive_service, file_name, directory):
-    files = []
-    page_token = None
+class DriveFilesSvc:
 
-    while True:
-        response = drive_service.files().list(
-            q=f"name='{file_name}' and trashed=false and '{directory}' in parents",
-            spaces='drive',
-            fields='nextPageToken, files(id, modifiedTime)',
-            pageToken=page_token
+    def __init__(self, drive_service):
+        self.drive_service = drive_service
+
+    @staticmethod
+    def get_filename_metadata(file_path):
+        return {'name': Path(file_path).name}
+
+    def get_files_by_name(self, file_name, directory):
+        files = []
+        page_token = None
+
+        while True:
+            response = self.drive_service.files().list(
+                q=f"name='{file_name}' and trashed=false and '{directory}' in parents",
+                spaces='drive',
+                fields='nextPageToken, files(id, size, modifiedTime)',
+                pageToken=page_token
+            ).execute()
+
+            for file in response.get('files', []):
+                files.append(file)
+
+            page_token = response.get('nextPageToken', None)
+            if page_token is None:
+                break
+
+        return files
+
+    def create_file(self, file_path, directory=None):
+        file_metadata = self.get_filename_metadata(file_path)
+        if directory:
+            file_metadata['parents'] = [directory]
+
+        self.drive_service.files().create(
+            body=file_metadata,
+            media_body=MediaFileUpload(file_path)
         ).execute()
 
-        for file in response.get('files', []):
-            files.append(file)
-
-        page_token = response.get('nextPageToken', None)
-        if page_token is None:
-            break
-
-    return files
-
-
-def get_newest_file_id(drive_service, file_name, directory):
-    files = get_files_by_name(drive_service, file_name, directory)
-    if not files:
-        return None
-
-    dates_list = []
-    for file in files:
-        modified_time = datetime.strptime(
-            file.get('modifiedTime'), '%Y-%m-%dT%H:%M:%S.%f%z')
-        dates_list.append(modified_time)
-
-    dates_list.sort()
-
-    for file in files:
-        modified_time = datetime.strptime(
-            file.get('modifiedTime'), '%Y-%m-%dT%H:%M:%S.%f%z')
-        if modified_time == dates_list[-1]:
-            return file.get('id')
-
-
-def create_file(drive_service, file_path, directory=None):
-    file_name = Path(file_path).name
-    file_metadata = {'name': file_name}
-    if directory:
-        file_metadata['parents'] = [directory]
-
-    drive_service.files().create(
-        body=file_metadata,
-        media_body=MediaFileUpload(file_path)
-    ).execute()
-
-
-def update_file(drive_service, file_path, file_id):
-    file_name = Path(file_path).name
-    file_metadata = {'name': file_name}
-    drive_service.files().update(
-        body=file_metadata,
-        media_body=MediaFileUpload(file_path),
-        fileId=file_id
-    ).execute()
+    def update_file(self, file_path, file_id):
+        self.drive_service.files().update(
+            body=self.get_filename_metadata(file_path),
+            media_body=MediaFileUpload(file_path),
+            fileId=file_id
+        ).execute()
