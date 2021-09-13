a0f642b41480cb4488de6e00d8ca41c8d6ff230c
vllblvck <m.komar12121998@gmail.com>
2021-09-13 14:11:41 +0200

    Refactored code and added doc strings

--- src/backup_files.py
+++ src/backup_files.py
@@ -1,11 +1,10 @@
-#!/usr/bin/python
-
-import config
-import googledrive as drive
-
 from datetime import datetime, timezone
 from glob import glob
 from pathlib import Path
+
+import config
+import googledrive as drive
+from googleapiclient.discovery import Resource
 
 
 SCOPES = ['https://www.googleapis.com/auth/drive.file']
@@ -15,64 +14,99 @@ TOKEN = AUTH_DIR / 'token.json'
 DATE_FORMAT = '%Y-%m-%dT%H:%M:%S.%f%z'
 
 
-def get_newest_file(drive_files):
+def get_newest_file(drive_files: list[dict]) -> dict:
+    """
+        Returns google drive file with newest modification time
+
+        Parameters:
+        drive_files (list[dict]): list of dictionaries containing google drive file metadata
+
+        Returns:
+        dict: dictionary containing google drive file metadata
+    """
+
     if not drive_files:
-        return None
+        return {}
 
     dates_list = []
+
     for file in drive_files:
-        modified_time = datetime.strptime(
-            file.get('modifiedTime'), DATE_FORMAT)
+        modified_time = datetime.strptime(file.get('modifiedTime'), DATE_FORMAT)
         dates_list.append(modified_time)
 
     dates_list.sort()
 
     for file in drive_files:
-        modified_time = datetime.strptime(
-            file.get('modifiedTime'), DATE_FORMAT)
+        modified_time = datetime.strptime(file.get('modifiedTime'), DATE_FORMAT)
         if modified_time == dates_list[-1]:
             return file
 
+    return {}
 
-def file_changed(file_path, drive_file):
-    file_metadata = Path(file_path).stat()
 
-    size_changed = int(drive_file.get('size')) != file_metadata.st_size
+def file_changed(local_file: str, drive_file: dict) -> bool:
+    """
+        Checks whether file located on google drive is different from the one on hard disk
+
+        Parameters:
+        local_file (str): system path to a file
+        drive_file (dict): google drive file metadata
+
+        Returns:
+        bool: whether files differ or not
+    """
+
+    local_metadata = Path(local_file).stat()
+
+    size_changed = int(drive_file.get('size')) != local_metadata.st_size
     if size_changed:
         return True
 
-    local_mtime = datetime.fromtimestamp(file_metadata.st_mtime, tz=timezone.utc)
+    local_mtime = datetime.fromtimestamp(local_metadata.st_mtime, tz=timezone.utc)
     drive_mtime = datetime.strptime(drive_file.get('modifiedTime'), DATE_FORMAT)
 
     return local_mtime > drive_mtime
 
 
-def upload_files(drive_svc, paths, directory):
+def upload_files(drive_svc: Resource, paths: list[str], directory: str) -> None:
+    """
+        Uploads files to google drive
+
+        Parameters:
+        drive_svc (Resource): object for interaction with google drive api
+        paths (list[str]): list of system paths
+        directory (str): google drive directory id
+
+        Returns:
+        None
+    """
+
     for path in paths:
         if Path(path).is_dir():
             continue
 
-        print('Checking whether file exists on google drive...')
-        file_name = Path(path).name
         files_svc = drive.DriveFilesSvc(drive_svc)
+        file_name = Path(path).name
+
+        print(f'Checking whether {file_name} exists on google drive...')
         drive_files = files_svc.get_files_by_name(file_name, directory)
         drive_file = get_newest_file(drive_files)
         drive_file_id = drive_file.get('id')
 
         if not drive_file_id:
             print(f'Creating {file_name} on google drive...')
-            files_svc.create_file(path, directory=directory)
+            files_svc.create_file(path, directory)
         elif file_changed(path, drive_file):
             print(f'Updating {file_name} on google drive...')
             files_svc.update_file(path, drive_file_id)
         else:
-            print('Skipping file update because it has not been modified')
+            print(f'Skipping {file_name} update because it has not been modified')
 
 
-def main():
+def main() -> None:
     try:
-        print('Authorizing...')
-        drive_service = drive.authorize(TOKEN, CREDENTIALS, SCOPES)
+        print('Authorizing')
+        drive_svc = drive.authorize(TOKEN, CREDENTIALS, SCOPES)
 
         for backup in config.BACKUPS:
             resolved_paths = glob(backup[0], recursive=True)
@@ -80,13 +114,13 @@ def main():
                 print(f'PATTERN \'{backup[0]}\' IS INVALID!!!')
                 continue
 
-            upload_files(drive_service, resolved_paths, backup[1])
+            upload_files(drive_svc, resolved_paths, backup[1])
 
         print('Done')
 
     except Exception as ex:
-        print('EXCEPTION OCCURED WHILE UPLOADING FILES!')
         print(ex)
+        print('EXCEPTION OCCURED WHILE UPLOADING FILES!')
 
 
 if __name__ == '__main__':
--- src/googledrive.py
+++ src/googledrive.py
@@ -1,5 +1,3 @@
-#!/usr/bin/python
-
 from pathlib import Path
 
 from google.auth.transport.requests import Request
@@ -9,42 +7,87 @@ from googleapiclient.http import MediaFileUpload
 from google.oauth2.credentials import Credentials
 
 
-def authorize(token_path, credentials_path, scopes):
+def authorize(token: str, credentials: str, scopes: list[str]) -> Resource:
+    """
+        Creates service for interaction with google drive
+
+        Parameters: 
+        token (str): system path to json file that contains authentication token
+        credentials (str): system path to json file that contains credentials for authentication with google drive
+        scopes (list[str]): list of permissions for the app on google drive
+
+        Returns:
+        Resource: object for interaction with google drive api
+    """
+
     creds = None
 
-    if Path(token_path).exists():
-        creds = Credentials.from_authorized_user_file(token_path, scopes)
+    if Path(token).exists():
+        creds = Credentials.from_authorized_user_file(token, scopes)
 
     if not creds or not creds.valid:
         if creds and creds.expired and creds.refresh_token:
             creds.refresh(Request())
         else:
-            flow = InstalledAppFlow.from_client_secrets_file(
-                credentials_path, scopes)
+            flow = InstalledAppFlow.from_client_secrets_file(credentials, scopes)
             creds = flow.run_local_server(port=0)
 
-        with open(token_path, 'w') as token:
+        with open(token, 'w') as token:
             token.write(creds.to_json())
 
     return build('drive', 'v3', credentials=creds)
 
 
 class DriveFilesSvc:
+    """
+        Class for interaction with files located on google drive
+    """
 
-    def __init__(self, drive_service):
-        self.drive_service = drive_service
+    def __init__(self, drive_svc: Resource) -> None:
+        """
+            Initializes class fields
+
+            Parameters:
+            drive_svc (Resource): object for interaction with google drive api
+
+            Returns:
+            None
+        """
+
+        self.drive_svc = drive_svc
 
     @staticmethod
-    def get_filename_metadata(file_path):
-        return {'name': Path(file_path).name}
+    def get_file_metadata(file: str) -> dict:
+        """
+            Returns file metadata containing file name
 
-    def get_files_by_name(self, file_name, directory):
+            Parameters:
+            file (str): system path to a file
+
+            Returns:
+            dict: dictionary containing key:'name' and value of file parameter
+        """
+
+        return {'name': Path(file).name}
+
+    def get_files_by_name(self, name: str, directory: str) -> list[dict]:
+        """
+            Returns list of files metadata that match specified name and directory
+
+            Parameters:
+            name (str): file name
+            directory (str): google drive directory id
+
+            Returns:
+            list: list of dictionaries containing file metadata (id, size, modifiedTime)
+        """
+
         files = []
         page_token = None
 
         while True:
-            response = self.drive_service.files().list(
-                q=f"name='{file_name}' and trashed=false and '{directory}' in parents",
+            response = self.drive_svc.files().list(
+                q=f"name='{name}' and trashed=false and '{directory}' in parents",
                 spaces='drive',
                 fields='nextPageToken, files(id, size, modifiedTime)',
                 pageToken=page_token
@@ -59,19 +102,37 @@ class DriveFilesSvc:
 
         return files
 
-    def create_file(self, file_path, directory=None):
-        file_metadata = self.get_filename_metadata(file_path)
-        if directory:
-            file_metadata['parents'] = [directory]
+    def create_file(self, file: str, directory: str) -> None:
+        """
+            Uploads file to google drive
 
-        self.drive_service.files().create(
+            Parameters:
+            file (str): system path to a file
+            directory (str): google drive directory id
+
+            Returns:
+            None
+        """
+
+        file_metadata = self.get_file_metadata(file)
+        file_metadata['parents'] = [directory]
+
+        self.drive_svc.files().create(
             body=file_metadata,
-            media_body=MediaFileUpload(file_path)
+            media_body=MediaFileUpload(file)
         ).execute()
 
-    def update_file(self, file_path, file_id):
-        self.drive_service.files().update(
-            body=self.get_filename_metadata(file_path),
-            media_body=MediaFileUpload(file_path),
-            fileId=file_id
+    def update_file(self, file: str, id: str) -> None:
+        """
+            Updates file on google drive
+
+            Parameters:
+            file (str): system path to a file
+            id (str): google drive file id
+        """
+
+        self.drive_svc.files().update(
+            body=self.get_file_metadata(file),
+            media_body=MediaFileUpload(file),
+            fileId=id
         ).execute()
